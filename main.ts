029428695324022fb6abd3f080dcbe9d48cb6345
DaveRayers <73789064+DaveRayers@users.noreply.github.com>
2020-11-01 16:10:47 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 let strip: neopixel.Strip = null
 maqueen.IR_callbackUser(function (message) {
     basic.showLeds(`
-        # . # # .
         # . # . #
-        # . # # .
-        # . # # .
+        # . # . #
+        # # # . #
+        # . # . #
         # . # . #
         `)
-    strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+    strip = neopixel.create(DigitalPin.P1, 12, NeoPixelMode.RGB)
     basic.pause(500)
     for (let index = 0; index <= 4; index++) {
         if (message == 28) {
@@ -18,5 +18,14 @@ maqueen.IR_callbackUser(function (message) {
         basic.pause(500)
         strip.showColor(neopixel.colors(NeoPixelColors.Red))
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
     }
+    basic.pause(500)
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
 })
