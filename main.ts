1d95b9dcb5605316e61750225c00b6275b7af307
DaveRayers <73789064+DaveRayers@users.noreply.github.com>
2020-11-01 15:57:56 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+let strip: neopixel.Strip = null
+maqueen.IR_callbackUser(function (message) {
+    basic.showLeds(`
+        # . # # .
+        # . # . #
+        # . # # .
+        # . # # .
+        # . # . #
+        `)
+    strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+    basic.pause(500)
+    for (let index = 0; index <= 4; index++) {
+        if (message == 28) {
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            strip.showRainbow(1, 360)
+        }
+        basic.showString("" + (message))
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    }
+})
